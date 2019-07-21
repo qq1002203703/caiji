c42c6d4646123d601015358546b94ca0e8fcc2d0
qq46502166 <1002203703@qq.com>
2019-07-21 09:59:43 +0800

    '20190721-1'

--- tpl/pc/fly/tmp/menu_main.php
+++ tpl/pc/fly/tmp/menu_main.php
@@ -1,6 +1,6 @@
 <li class="layui-nav-item"><a href="/">首页</a></li>
 <li class="layui-nav-item"><a href="/group/all">小组</a></li>
-<li class="layui-nav-item"><a href="/user/all">会员中心/a></li>
+<li class="layui-nav-item"><a href="/user/all">会员中心</a></li>
 <li class="layui-nav-item"><a href="#">关于本站</a></li>
 <li class="layui-nav-item" style="padding-right: 10px"><a href="/api/open/go?l=<?=\extend\Helper::urlencode('https://www.2hz001.com')?>" class="layui-btn layui-btn-danger layui-btn-sm" rel="nofollow" target="_blank">1号站登陆</a></li>
 <li class="layui-nav-item"  style="padding-right: 10px"><a href="/api/open/go?l=<?=\extend\Helper::urlencode('https://www.2hz001.com')?>" class="layui-btn layui-btn-sm" rel="nofollow" target="_blank">2号站登陆</a></li>
